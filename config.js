bdf7d22a11d6213d7f862bb376003e2ce83a93e4
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2017-10-08 08:48:04 +0200

    fix for connecting to communicator

--- config.js
+++ config.js
@@ -4,4 +4,11 @@ module.exports = {
 	cacheEnabled: false,
 	cacheDuration: 3600000,
 	db_connectionString: 'mongodb://user-service-user:user-service-user@ds015636.mlab.com:15636/heroku_q9zz0x8s',
+	pulse: {
+		shouldRegister:true,
+		serviceName: "uptiverse-news",
+		path:"/api/pulse",
+		communicatorUrl: process.env.COMMUNICATOR_URL || "",
+		token: process.env.COMMUNICATOR_TOKEN || ""
+	}
 };
