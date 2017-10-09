f46dbfad123c95861fd4124787550751cd0b3f4f
ulfdahlstrand <ulf.d.dahlstrand@gmail.com>
2017-10-09 22:12:14 +0200

    changed config after rework of communicator setup in ms-core

--- config.js
+++ config.js
@@ -4,12 +4,16 @@ module.exports = {
 	cacheEnabled: false,
 	cacheDuration: 3600000,
 	db_connectionString: 'mongodb://user-service-user:user-service-user@ds015636.mlab.com:15636/heroku_q9zz0x8s',
-	pulse: {
-		shouldRegister:true,
-		serviceName: "uptiverse-news",
-		host: process.env.SERVICE_URL || "",
-		path:"/api/pulse",
-		communicatorUrl: process.env.COMMUNICATOR_URL || "",
+	communicator:{
+		service: {
+			name: "uptiverse-news",
+			host: process.env.SERVICE_URL || ""
+		},
+		path: process.env.COMMUNICATOR_PATH || "",
 		token: process.env.COMMUNICATOR_TOKEN || ""
+	},
+	pulse: {
+		shouldRegister: true,
+		path:"/api/pulse"
 	}
 };
